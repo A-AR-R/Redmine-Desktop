4b6a8e89165062f6e2e4a0c581eac6978a6a9ea8
a.alipour <amir.alipour.r@gmail.com>
2021-01-30 18:39:35 +0330

    [feat] update card styles

--- src/Pages/AgileBoard.js
+++ src/Pages/AgileBoard.js
@@ -11,12 +11,20 @@ import ReactMde from "react-mde";
 import "react-mde/lib/styles/css/react-mde-all.css";
 import ReactMarkdown from "react-markdown";
 import * as Showdown from "showdown";
+import styled, {createGlobalStyle, css} from 'styled-components'
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        height: '100%',
-    }
-}));
+const MyScrollableLane = styled.div`
+  flex: 1;
+  overflow-y: auto;
+  overflow-x: hidden;
+  align-self: center;
+  width:100%;
+  max-height: 90vh;
+  margin-top: 10px;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
 const MyCard = props => {
     const [selectedTab, setSelectedTab] = React.useState("preview");
     const [state, setState] = useState(props)
@@ -31,7 +39,7 @@ const MyCard = props => {
 
     const handleTabChange = (tab) => {
         setSelectedTab(tab)
-        if(tab==='preview'){
+        if (tab === 'preview') {
             axios({
                 method: 'put',
                 url: "http://" + mainState.serverName + "/issues/" + state.id + ".json",
@@ -46,7 +54,8 @@ const MyCard = props => {
                     "Access-Control-Allow-Headers": "X-Requested-With",
                     'X-Redmine-API-Key': mainState.token_id,
                     'Access-Control-Allow-Origin': '*'
-                }}).then(res => {
+                }
+            }).then(res => {
                     console.log(res)
 
                 }
@@ -62,20 +71,18 @@ const MyCard = props => {
     }
 
     return (
-        <div>
+        <div style={{border: '1px solid', minWidth: 'unset'}}>
             <header
                 style={{
-                    borderBottom: '1px solid #eee', paddingBottom: 6, marginBottom: 10,
+                    borderBottom: '1px solid #eee', height: "2.5rem",
                     display: 'flex', flexDirection: 'row', justifyContent: 'space-between',
-                    color: props.cardColor
+                    background: props.cardColor
                 }}
             >
                 <div style={{fontSize: 14, fontWeight: 'bold'}}>{props.title}</div>
                 <div style={{fontSize: 11}}>{props.label}</div>
             </header>
             <div style={{fontSize: 12, color: '#BD3B36'}}>
-                <div style={{color: '#4C4C4C', fontWeight: 'bold'}}>{props.subTitle}</div>
-                <div style={{padding: '5px 0px'}}><i>{props.body}</i></div>
                 <ReactMde
                     value={state.description}
                     onChange={handleChange}
@@ -97,6 +104,7 @@ const MyCard = props => {
 }
 
 const components = {
+    ScrollableLane: MyScrollableLane,
     Card: MyCard,
 };
 
@@ -174,11 +182,18 @@ class AgileBoard extends Component {
                 var data = {
                     lanes: []
                 }
+                let colors = ['#2196F3', '#BA68C8', '#43A047']
+                let count = res.data.issue_statuses.length
                 res.data.issue_statuses.forEach(function (issue_status) {
                     data.lanes.push({
                         id: issue_status.id.toString(),
                         title: issue_status.name,
-                        style: {width: 280},
+                        //FIXME remove hardcoded window size(960)
+                        style: {
+                            width: (960 / count) + 'px',
+                            backgroundColor: 'transparent',
+                            border: '1px solid ' + colors.pop()
+                        },
                         cards: []
                     })
                 })
@@ -206,7 +221,9 @@ class AgileBoard extends Component {
             }
 
         }).then(res => {
+                let colors = ['#00CC6A', '#FF8C00', '#E74856']
                 res.data.issues.forEach(function (issue) {
+                    console.log(issue.priority.id)
                     self.eventBus.publish({
                         type: 'ADD_CARD',
                         laneId: issue.status.id.toString(),
@@ -214,7 +231,8 @@ class AgileBoard extends Component {
                             id: issue.id.toString(),
                             title: issue.subject,
                             label: issue.tracker.name,
-                            description: issue.description
+                            description: issue.description,
+                            cardColor: colors[issue.priority.id-1]
                         }
                     })
                 })
@@ -227,9 +245,12 @@ class AgileBoard extends Component {
         const {data} = this.state;
         return (
             <div className="height100">
-                {!isEmpty(data) ? <Board style={{textAlign:'left'}} className="height100" data={data} components={components}
-                                         onCardMoveAcrossLanes={this.onCardMoveAcrossLanes}
-                                         eventBusHandle={this.setEventBus} draggable/> : <p>Loading...</p>}
+                {!isEmpty(data) ?
+                    <Board style={{backgroundColor: 'whitesmoke', textAlign: 'left', width: '100%'}}
+                           className="height100" data={data}
+                           components={components}
+                           onCardMoveAcrossLanes={this.onCardMoveAcrossLanes}
+                           eventBusHandle={this.setEventBus} draggable/> : <p>Loading...</p>}
             </div>
         );
     }
